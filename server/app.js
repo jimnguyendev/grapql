7fbe61a94480c4890b6975fcd387149020bd67aa
tomosianguyennam <nam.nguyen1@tomosia.com>
2022-04-05 13:46:07 +0700

    resolver

--- server/app.js
+++ server/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
 // Load schema resolvers
 const typeDefs = require('./schema/schema');
 const resolvers = require('./resolver/resolver');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+// Load db methods
+const mongoDataMethods = require('./data/db')
+
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://nam-nguyen:1234@graphql.ubi6f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error.message)
+
+    process.exit(1)
+  }
+}
+
+connectDB()
 
 const app = express();
 
@@ -16,6 +33,7 @@ async function startServer() {
   apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
+    context: () => ({ mongoDataMethods })
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
@@ -23,6 +41,6 @@ async function startServer() {
 startServer().catch(err => console.log(err));
 
 app.listen({ port: 4000 }, () => {
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+  console.log(`Server ready at http://localhost:4000${apolloServer.graphqlPath}`);
 });
 
--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,27 +1,36 @@
 const {books, authors} = require("../data/static");
+const Author = require('../models/Author');
+const Book = require('../models/Book');
 const resolvers = {
   // Query
   Query: {
-    books: () => books,
+    books: async (parent, args, { mongoDataMethods }) =>
+        await mongoDataMethods.getAllBooks(),
     book: (parent, args) => books.find(book => book.id == args.id),
     authors: () => authors,
     author: (parent, args) => authors.find(author => author.id == args.id),
   },
   Book: {
     author: (parent, args) => {
-      return authors.find(author => author.id === parent.authorId)
+      return authors.find(author => author.id == parent.authorId)
     }
   },
   Author: {
     books: (parent, args) => {
-      return books.filter(book => book.authorId === parent.id)
+      return books.filter(book => book.authorId == parent.id)
     }
   },
 
   // mutation
   Mutation: {
-    createAuthor: (parent, args) => args,
-    createBook: (parent, args) => args
+    createAuthor: async (parent, args) => {
+      const newAuthor = new Author(args);
+      return await newAuthor.save();
+    },
+    createBook: async (parent, args) => {
+      const newBook = new Book(args);
+      return await newBook.save();
+    }
   }
 }
 
